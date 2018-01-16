ead8b674cc48f728287930735b815230f318de60
moobsen <moobsen@gmail.com>
2018-01-16 14:23:36 +0100

    removed moved files

--- parse_log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#extract all lat and lon as csv from logfile
-LOGFILE="log-follow.log"
-grep lat $LOGFILE | grep -o "[0-9]*\.[0-9]*" | tr '\n' ',' | sed 's/[0-9]\.[0-9]\{2,3\}\,/\n/g'
--- run-fake-gps.sh
+++
@@ -1,2 +0,0 @@
-echo Sending fake GPS data
-gpsfake -c 0.05 gpsdata/gps-trace3.log
